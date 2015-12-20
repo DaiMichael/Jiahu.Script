1b02ce3ec50857a129107548f36ed179f651e399
Dai Michael <david.llewellyn.michael@gmail.com>
2015-12-20 22:47:40 +0000

    Adding missing files

---
+++ JiahuScript.External.Examples/Functions/AddNumbersFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JiahuScriptRuntime.External;
+using JiahuScriptRuntime.External.Attributes;
+
+namespace JiahuScript.External.Examples.Functions
+{
+    [Function("Add", "Maths", "Adds two numbers together.")]
+    [Function("AddTogether", "Legacy", "Adds two numbers together.")]
+    [Parameter(1, "first", typeof(int))]
+    [Parameter(2, "second", typeof(int))]
+    [ReturnType(typeof(int))]
+    public class AddNumbersFunction : FunctionBase
+    {
+        public override object Eval(string functionName, object[] parameters)
+        {
+            CheckParametersAndThrowOnMissMatch(parameters);
+            return (int)parameters[0] + (int)parameters[1];
+        }
+    }
+}
---
+++ JiahuScript.External.Examples/Functions/ToUpperFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JiahuScriptRuntime.External;
+using JiahuScriptRuntime.External.Attributes;
+
+namespace JiahuScript.External.Examples.Functions
+{
+    [Function("ToUpper", "Text", "Will change text to uppercase.")]
+    [Parameter(1, "text", typeof(string))]
+    [ReturnType(typeof(string))]
+    public class ToUpperFunction : FunctionBase
+    {
+        public override object Eval(string functionName, object[] parameters)
+        {
+            CheckParametersAndThrowOnMissMatch(parameters);
+            return parameters[0].ToString().ToUpper();
+        }
+    }
+}
